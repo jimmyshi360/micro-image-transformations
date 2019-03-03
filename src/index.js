95fd66915028df5934aee12983bbc0827e5b9e1d
Jimmy Shi <jimmyshi360@gmail.com>
2019-03-03 11:09:24 -0500

    added eslint, linted code addded lint to pretest

--- src/index.js
+++ src/index.js
@@ -1,85 +1,91 @@
-
-
 module.exports = {
 
-    /** Accepts an Image object and returns the modified grayscale version of the image.
-        Uses the luminosity method by doing 0.21 R + 0.72 G + 0.07 B on each pixel(It looks nicer!).
-     **/
-    grayscale: function(image) {
+  /** Accepts an Image object and returns the modified grayscale version of the image.
+     Uses the luminosity method by doing 0.21 R + 0.72 G + 0.07 B on each pixel(It looks nicer!).
+     * */
+  grayscale(image) {
+    // uses the canvas method for manipulating images
+    const canvas = document.createElement('canvas');
+    const canvasContext = canvas.getContext('2d');
 
-            //uses the canvas method for manipulating images
-        	let canvas = document.createElement('canvas');
-        	let canvasContext = canvas.getContext('2d');
+    const imgWidth = image.width;
+    const imgHeight = image.height;
+    if (imgWidth === 0 || imgHeight === 0) {
+      throw new Error('cannot process empty image');
+    }
+    canvas.width = imgWidth;
+    canvas.height = imgHeight;
 
-        	let imgWidth = image.width;
-        	let imgHeight = image.height;
-        	if(imgWidth ===0 || imgHeight ===0)
-        	    throw "cannot process empty image";
-        	canvas.width = imgWidth;
-        	canvas.height = imgHeight;
+    canvasContext.drawImage(image, 0, 0);
+    const pixelGrid = canvasContext.getImageData(0, 0, imgWidth, imgHeight);
+    for (let y = 0; y < pixelGrid.height; y += 1) {
+      for (let x = 0; x < pixelGrid.width; x += 1) {
+        // calculate pixel index to manipulate
+        // We multiply by 4 because pixel data is stored (r,g,b,a) so we traverse 4 blocks at a time
+        const i = (y * 4) * pixelGrid.width + x * 4;
+        // average values based on the luminosity equation
+        const R = pixelGrid.data[i];
+        const G = pixelGrid.data[i + 1];
+        const B = pixelGrid.data[i + 2];
+        const avg = (R * 0.21 + G * 0.72 + B * 0.07);
+        pixelGrid.data[i] = avg;
+        pixelGrid.data[i + 1] = avg;
+        pixelGrid.data[i + 2] = avg;
+      }
+    }
+    // insert modified image and convert into an img element to return
+    canvasContext.putImageData(pixelGrid, 0, 0, 0, 0, pixelGrid.width, pixelGrid.height);
+    const output = new Image();
+    output.src = canvas.toDataURL();
+    return output;
+  },
 
-        	canvasContext.drawImage(image, 0, 0);
-        	let pixelGrid = canvasContext.getImageData(0, 0, imgWidth, imgHeight);
-        	for (y = 0; y < pixelGrid.height; y++) {
-        		for (x = 0; x < pixelGrid.width; x++) {
-        		    //calculate pixel index to manipulate
-        		    //We multiply by 4 because pixel data is stored (r,g,b,a) so we traverse 4 blocks at a time
-        			let i = (y * 4) * pixelGrid.width + x * 4;
-        			//average values based on the luminosity equation
-        			let avg = (pixelGrid.data[i]*0.21 + pixelGrid.data[i + 1]*0.72 + pixelGrid.data[i + 2]*0.07) ;
-        			pixelGrid.data[i] = avg;
-        			pixelGrid.data[i + 1] = avg;
-        			pixelGrid.data[i + 2] = avg;
-        		}
-        	}
-        	//insert modified image and convert into an img element to return
-        	canvasContext.putImageData(pixelGrid, 0, 0, 0, 0, pixelGrid.width, pixelGrid.height);
-        	let output = new Image();
-        		output.src = canvas.toDataURL();
-        	return output;
-    },
+  /** Crops an image. cropX and cropY specify the bottom left corner to begin cropping
+     cropW is the width to crop and cropH is the height to crop.
+     * */
+  crop(image, startX, startY, cropWidth, cropHeight) {
+    // uses the canvas method for manipulating images
+    const canvas = document.createElement('canvas');
+    const canvasContext = canvas.getContext('2d');
 
-    /** Crops an image. cropX and cropY specify the bottom left corner to begin cropping
-        cropW is the width to crop and cropH is the height to crop.
-         **/
-    crop: function(image, cropX, cropY, cropW, cropH) {
+    const imgWidth = image.width;
+    const imgHeight = image.height;
+    const cropX = startX;
+    const cropY = imgHeight - startY;
+    const cropW = cropWidth;
+    const cropH = cropHeight;
+    if (imgWidth === 0 || imgHeight === 0) {
+      throw new Error('cannot process empty image');
+    }
+    canvas.width = imgWidth;
+    canvas.height = imgHeight;
 
-                           //uses the canvas method for manipulating images
-        	let canvas = document.createElement('canvas');
-        	let canvasContext = canvas.getContext('2d');
+    canvasContext.drawImage(image, 0, 0);
+    const pixelGrid = canvasContext.getImageData(0, 0, imgWidth, imgHeight);
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.width = cropW;
+    canvas.height = cropH;
+    const pixelGridCropped = canvasContext.createImageData(cropW, cropH);
 
-        	let imgWidth = image.width;
-        	let imgHeight = image.height;
-        	cropY=imgHeight-cropY;
-        	if(imgWidth ===0 || imgHeight ===0)
-        	    throw "cannot process empty image";
-        	canvas.width = imgWidth;
-        	canvas.height = imgHeight;
-
-        	canvasContext.drawImage(image, 0, 0);
-        	let pixelGrid = canvasContext.getImageData(0, 0, imgWidth, imgHeight);
-			canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-			canvas.width = cropW;
-        	canvas.height = cropH;
-			let pixelGridCropped = canvasContext.createImageData(cropW, cropH);
-			
-        	for (y = cropY-cropH; y < cropY; y++) {
-        		for (x = cropX; x < cropX+cropW; x++) {
-        		    //calculate pixel index to manipulate
-        		    //We multiply by 4 because pixel data is stored (r,g,b,a) so we traverse 4 blocks at a time
-        			let i = (y * 4) * pixelGrid.width + x * 4;
-					let cropI = ((y-cropY+cropH) * 4) * cropW + (x-cropX) * 4;
-        			//average values based on the luminosity equation
-        			pixelGridCropped.data[cropI] = pixelGrid.data[i];
-        			pixelGridCropped.data[cropI + 1] = pixelGrid.data[i+1];
-        			pixelGridCropped.data[cropI + 2] = pixelGrid.data[i+2];
-					pixelGridCropped.data[cropI + 3] = 255;
-        		}
-        	}
-        	//insert modified image and convert into an img element to return
-        	canvasContext.putImageData(pixelGridCropped, 0, 0, 0, 0, pixelGridCropped.width, pixelGridCropped.height);
-        	let output = new Image();
-        		output.src = canvas.toDataURL();
-        	return output;
-        }
+    for (let y = cropY - cropH; y < cropY; y += 1) {
+      for (let x = cropX; x < cropX + cropW; x += 1) {
+        // calculate pixel index to manipulate
+        // We multiply by 4 because pixel data is stored (r,g,b,a) so we traverse 4 blocks at a time
+        const i = (y * 4) * pixelGrid.width + x * 4;
+        const cropI = ((y - cropY + cropH) * 4) * cropW + (x - cropX) * 4;
+        // average values based on the luminosity equation
+        pixelGridCropped.data[cropI] = pixelGrid.data[i];
+        pixelGridCropped.data[cropI + 1] = pixelGrid.data[i + 1];
+        pixelGridCropped.data[cropI + 2] = pixelGrid.data[i + 2];
+        pixelGridCropped.data[cropI + 3] = 255;
+      }
+    }
+    // insert modified image and convert into an img element to return
+    const cWidth = pixelGridCropped.width;
+    const cHeight = pixelGridCropped.height;
+    canvasContext.putImageData(pixelGridCropped, 0, 0, 0, 0, cWidth, cHeight);
+    const output = new Image();
+    output.src = canvas.toDataURL();
+    return output;
+  },
 };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,13 +1,16 @@
-let expect = require('chai').expect;
-let imageTransformations = require('../src/index');
-let Canvas = require("canvas");
+const { expect } = require('chai');
+const Canvas = require('canvas');
+const { describe, it } = require('mocha');
+
 global.Image = Canvas.Image;
 const {
-	JSDOM
+  JSDOM,
 } = require('jsdom');
+const imageTransformations = require('../src/index');
+
 const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
 const {
-	window
+  window,
 } = jsdom;
 
 
@@ -15,135 +18,118 @@ global.window = window;
 global.document = window.document;
 
 // Utility function for diffing images
-//image diff is necessary as directly comparing Base64 is unreliable due to varying encoding schemes
-function imageDiff (img1, img2) {
+// image diff is necessary as
+// directly comparing Base64 is unreliable due to varying encoding schemes
+function imageDiff(img1, img2) {
+  const canvas1 = document.createElement('canvas');
+  const canvasContext1 = canvas1.getContext('2d');
 
-	let canvas1 = document.createElement('canvas');
-	let canvasContext1 = canvas1.getContext('2d');
+  const canvas2 = document.createElement('canvas');
+  const canvasContext2 = canvas2.getContext('2d');
 
-	let canvas2 = document.createElement('canvas');
-	let canvasContext2 = canvas2.getContext('2d');
+  const imgWidth1 = img1.width;
+  const imgHeight1 = img1.height;
 
-	let imgWidth1 = img1.width;
-	let imgHeight1 = img1.height;
+  const imgWidth2 = img2.width;
+  const imgHeight2 = img2.height;
 
-	let imgWidth2 = img2.width;
-	let imgHeight2 = img2.height;
+  canvas1.width = imgWidth1;
+  canvas1.height = imgHeight1;
 
-	canvas1.width = imgWidth1;
-	canvas1.height = imgHeight1;
+  canvas2.width = imgWidth2;
+  canvas2.height = imgHeight2;
 
-	canvas2.width = imgWidth2;
-	canvas2.height = imgHeight2;
+  canvasContext1.drawImage(img1, 0, 0);
+  canvasContext2.drawImage(img2, 0, 0);
 
-	canvasContext1.drawImage(img1, 0, 0);
-	canvasContext2.drawImage(img2, 0, 0);
+  const pixelGrid1 = canvasContext1.getImageData(0, 0, imgWidth1, imgHeight1);
+  const pixelGrid2 = canvasContext2.getImageData(0, 0, imgWidth2, imgHeight2);
 
-	let pixelGrid1 = canvasContext1.getImageData(0, 0, imgWidth1, imgHeight1);
-	let pixelGrid2 = canvasContext2.getImageData(0, 0, imgWidth2, imgHeight2);
+  for (let y = 0; y < pixelGrid1.height; y += 1) {
+    for (let x = 0; x < pixelGrid1.width; x += 1) {
+      const i = (y * 4) * pixelGrid1.width + x * 4;
 
-	for (y = 0; y < pixelGrid1.height; y++) {
-		for (x = 0; x < pixelGrid1.width; x++) {
+      // no pixels should be red
+      if (pixelGrid1.data[i] !== pixelGrid2.data[i]) { return false; }
+      if (pixelGrid1.data[i + 1] !== pixelGrid2.data[i + 1]) { return false; }
+      if (pixelGrid1.data[i + 2] !== pixelGrid2.data[i + 2]) { return false; }
+    }
+  }
+  return true;
+}
 
-			let i = (y * 4) * pixelGrid1.width + x * 4;
+describe('micro-image-transformations', () => {
+  describe('grayscale', () => {
+    // uses a red square to check that all pixels are updated,
+    // grayscale does not always change all pixels but it should still operate over all of them
+    it('should update/check all pixels', () => {
+      const imageNotRed = (img) => {
+        const canvas = document.createElement('canvas');
+        const canvasContext = canvas.getContext('2d');
 
-			//no pixels should be red
-			if (pixelGrid1.data[i] !== pixelGrid2.data[i])
-				return false;
-			if (pixelGrid1.data[i + 1] !== pixelGrid2.data[i + 1])
-				return false;
-			if (pixelGrid1.data[i + 2] !== pixelGrid2.data[i + 2])
-				return false;
-		}
-	}
-	return true;
+        const imgWidth = img.width;
+        const imgHeight = img.height;
+        canvas.width = imgWidth;
+        canvas.height = imgHeight;
+        canvasContext.drawImage(img, 0, 0);
 
-};
+        const pixelGrid = canvasContext.getImageData(0, 0, imgWidth, imgHeight);
 
-describe('micro-image-transformations', function () {
+        for (let y = 0; y < pixelGrid.height; y += 1) {
+          for (let x = 0; x < pixelGrid.width; x += 1) {
+            const i = (y * 4) * pixelGrid.width + x * 4;
 
-	describe('grayscale', function () {
+            // no pixels should be red
+            if (pixelGrid.data[i] === 255) { return false; }
+            if (pixelGrid.data[i + 1] === 255) { return false; }
+            if (pixelGrid.data[i + 2] === 255) { return false; }
+          }
+        }
+        return true;
+      };
 
-		//uses a red square to check that all pixels are updated,
-		//grayscale does not always change all pixels but it should still operate over all of them
-		it('should update/check all pixels', function () {
+      const redSquare = new Image();
+      // preload a (255, 0, 0) rgb red 16x16 square.
+      // Grayscale should change all pixels to another color
+      redSquare.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAHUlEQVQ4jWP8z8Dwn4ECwESJ5lEDRg0YNWAwGQAAWG0CHpmX3bgAAAAASUVORK5CYII=';
 
-			let imageNotRed = (img) => {
+      const grayscaleIMG = imageTransformations.grayscale(redSquare);
+      expect(imageNotRed(grayscaleIMG)).to.equal(true);
+    });
 
-				let canvas = document.createElement('canvas');
-				let canvasContext = canvas.getContext('2d');
+    // tests the precondition that images of size 1x1 or greater should work
+    it('should handle 1x1 images', () => {
+      const img = new Image();
+      // preload a (255, 0, 0) rgb red 1x1 square.
+      // Grayscale should change all pixels to another color
+      img.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWP4z8DwHwAFAAH/q842iQAAAABJRU5ErkJggg==';
+      try {
+        imageTransformations.grayscale(img);
+        expect(true).to.equal(true);
+      } catch (e) {
+        expect(false).to.equal(true);
+      }
+    });
 
-				let imgWidth = img.width;
-				let imgHeight = img.height;
-				canvas.width = imgWidth;
-				canvas.height = imgHeight;
-				canvasContext.drawImage(img, 0, 0);
+    // tests the precondition that empty image objects
+    // should result in an exception being thrown (width or height of 0)
+    it('should gracefully handle empty images', () => {
+      const img = new Image();
+      // preload a (255, 0, 0) rgb red 1x1 square.
+      // Grayscale should change all pixels to another color
+      expect(() => imageTransformations.grayscale(img)).to.throw('cannot process empty image');
+    });
 
-				let pixelGrid = canvasContext.getImageData(0, 0, imgWidth, imgHeight);
-
-				for (y = 0; y < pixelGrid.height; y++) {
-					for (x = 0; x < pixelGrid.width; x++) {
-
-						let i = (y * 4) * pixelGrid.width + x * 4;
-
-						//no pixels should be red
-						if (pixelGrid.data[i] === 255)
-							return false;
-						if (pixelGrid.data[i + 1] === 255)
-							return false;
-						if (pixelGrid.data[i + 2] === 255)
-							return false;
-					}
-				}
-				return true;
-
-			};
-
-			let redSquare = new Image();
-			//preload a (255, 0, 0) rgb red 16x16 square. Grayscale should change all pixels to another color
-			redSquare.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAHUlEQVQ4jWP8z8Dwn4ECwESJ5lEDRg0YNWAwGQAAWG0CHpmX3bgAAAAASUVORK5CYII=';
-
-			let grayscaleIMG = imageTransformations.grayscale(redSquare);
-			expect(imageNotRed(grayscaleIMG)).to.equal(true);
-
-		});
-
-		//tests the precondition that images of size 1x1 or greater should work
-		it('should handle 1x1 images', function () {
-
-			let img = new Image();
-			//preload a (255, 0, 0) rgb red 1x1 square. Grayscale should change all pixels to another color
-			img.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWP4z8DwHwAFAAH/q842iQAAAABJRU5ErkJggg==';
-			try {
-				let grayscaleIMG = imageTransformations.grayscale(img);
-				expect(true).to.equal(true);
-			} catch (e) {
-				expect(false).to.equal(true);
-			}
-
-		});
-
-		//tests the precondition that empty image objects should result in an exception being thrown (width or height of 0)
-		it('should gracefully handle empty images', function () {
-
-			let img = new Image();
-			//preload a (255, 0, 0) rgb red 1x1 square. Grayscale should change all pixels to another color
-
-			expect(() => imageTransformations.grayscale(img)).to.throw("cannot process empty image");
-
-		});
-
-		//optional test, assuming same grayscale logic is used. tests that the luminosity equation is accurate
-		it('(OPTIONAL) should calculate correct grayscale luminosity', function () {
-
-			let img = new Image();
-			//preload a (255, 0, 0) rgb red 1x1 square.
-			img.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWP4z8DwHwAFAAH/q842iQAAAABJRU5ErkJggg==';
-			let targetImage = new Image();
-			//preload the expected 1x1 image based on the luminosity equation
-			targetImage.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYV2MwMzP7DwAC6gGi4bvgyAAAAABJRU5ErkJggg==';
-			console.log(imageTransformations.grayscale(img).src);
-			expect(imageDiff(imageTransformations.grayscale(img), targetImage)).to.equal(true);
-		});
-	})
+    // optional test, assuming same grayscale logic is used.
+    // tests that the luminosity equation is accurate
+    it('(OPTIONAL) should calculate correct grayscale luminosity', () => {
+      const img = new Image();
+      // preload a (255, 0, 0) rgb red 1x1 square.
+      img.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQImWP4z8DwHwAFAAH/q842iQAAAABJRU5ErkJggg==';
+      const targetImage = new Image();
+      // preload the expected 1x1 image based on the luminosity equation
+      targetImage.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYV2MwMzP7DwAC6gGi4bvgyAAAAABJRU5ErkJggg==';
+      expect(imageDiff(imageTransformations.grayscale(img), targetImage)).to.equal(true);
+    });
+  });
 });
